5912a8cc08015668e8d71d895e54f1cd9965bc49
AMIT KUMAR <130242213+amitkumar97097@users.noreply.github.com>
2025-08-14 17:52:13 +0530

    Create cloudinary.js

---
+++ backend/utils/cloudinary.js
@@ -0,0 +1,18 @@
+import { v2 as cloudinary } from "cloudinary";
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const storage = new CloudinaryStorage({
+  cloudinary,
+  params: { folder: "real-estate-app", allowed_formats: ["jpg", "png", "jpeg", "webp"] },
+});
+
+export const uploader = multer({ storage });
+
+---
