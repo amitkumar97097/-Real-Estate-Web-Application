9fee25d8eb7aa98c830fec820a571076253a77c7
AMIT KUMAR <130242213+amitkumar97097@users.noreply.github.com>
2025-08-14 17:48:43 +0530

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["buyer", "seller", "agent", "admin"], default: "buyer" },
+    savedProperties: [{ type: mongoose.Schema.Types.ObjectId, ref: "Property" }],
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.compare = function (pw) {
+  return bcrypt.compare(pw, this.password);
+};
+
+export default mongoose.model("User", userSchema);
+
+---
