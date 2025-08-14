3f3ca6173df443f94d279f293c57391593cc5cef
AMIT KUMAR <130242213+amitkumar97097@users.noreply.github.com>
2025-08-14 18:03:04 +0530

    Create _app.jsx

---
+++ frontend/pages/_app.jsx
@@ -0,0 +1,15 @@
+import "../styles/globals.css";
+import Navbar from "../components/Navbar";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <div className="min-h-screen bg-slate-50 text-slate-900">
+      <Navbar />
+      <main className="max-w-6xl mx-auto px-4 py-6">
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
+}
+
+---
